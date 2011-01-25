a181a4ee34b2e7bbb26bc9547ddbccd3b72c527f
Jared Hoyt <jhoyt@tekniq.com>
2011-01-25 13:25:22 -0600

    Adding restore, disableDeletable, enableDeletable methods as well as 'isDeleted' query option

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### Please Note
-This behavior does not yet work with HABTM associations. Please feel free to fork and update!
+This behavior does not yet work with HABTM associations. Please feel free to fork and update! 
--- models/behaviors/soft_deletable.php
+++ models/behaviors/soft_deletable.php
@@ -1,43 +1,54 @@
 <?php
 /**
  * @todo Support HasAndBelongsToMany relationships.
+ * @todo Support more column types than boolean and datetime.
+ * @todo Support cascading restores.
  */
 class SoftDeletableBehavior extends ModelBehavior {
 	var $field = 'deleted';
+	var $settings = array();
 /**
  * Setup method for behavior instatiation - merge model settings.
  * 
- * @param object $Model Model instance
+ * @param object $model Model instance
  * @param array $settings Settings passed in from model
  */	
-	function setup(&$Model) {	
+	function setup(&$model) {
+		$this->enableDeletable($model);
+		
 		# Set associated model conditions
 		foreach (array('hasOne', 'hasMany', 'belongsTo') as $type) {
-			foreach ($Model->$type as $modelName => &$params) {	
-				$Association = $Model->$modelName;
+			foreach ($model->$type as $modelName => &$params) {
+				if (!isset($model->$modelName)) {
+					continue;
+				}
 				
-				if (in_array('SoftDeletable', (array)$Association->actsAs) && $Association->hasField($this->field)) {
-					$params['conditions'][$modelName . '.' . $this->field] = '0';
+				$association = $model->$modelName;
+				if (in_array('SoftDeletable', (array)$association->actsAs) && $association->hasField($this->field)) {
+					$params['conditions'][$modelName . '.' . $this->field] = $this->notDeleted($association);
 				}
 			}
 		}
 	}
 /**
- * When a record is deleted, set 'deleted' field to true and cancel hard delete.
+ * When a record is deleted, set 'deleted' field to true and cancel hard delete. If a
+ *  record has already been soft deleted, continue with the hard delete.
  * 
- * @param object $Model Model instance
+ * @param object $model Model instance
  * @param boolean $cascade True if model cascades deletes
  * @return boolean True if 'deleted' field doesn't exist (continue with hard delete)
  */	
-	function beforeDelete(&$Model, $cascade) {
-		if ($Model->hasField($this->field)) {
-			$id = $Model->id;	
-			
-			if ($Model->save(array($Model->alias => array($this->field => 'true')), array('validate' => false, 'callbacks' => false)) && $cascade) {
-				$Model->_deleteDependent($id, $cascade);
-				$Model->_deleteLinks($id);
+	function beforeDelete(&$model, $cascade) {
+		if ($model->hasField($this->field)) {
+			if (!$this->settings[$model->name]['enabled'] || $model->field($this->field) != $this->notDeleted($model)) {
+				return true;
 			}
-			$Model->afterDelete();		
+			
+			if ($this->update($model, $this->deleted($model)) && $cascade) {
+				$model->_deleteDependent($model->id, $cascade);
+				$model->_deleteLinks($model->id);
+			}
+			$model->afterDelete();		
 			return false;
 		}		
 		return true;
@@ -45,15 +56,104 @@ class SoftDeletableBehavior extends ModelBehavior {
 /**
  * Filter out deleted records when searching with Model::find().
  * 
- * @param object $Model Model instance
+ * @param object $model Model instance
  * @param boolean $queryData Information about current query: conditions, fields, etc.
  * @return boolean True if 'deleted' field doesn't exist (continue with hard delete)
  */		
-	function beforeFind($Model, $queryData) {
-		if ($Model->hasField($this->field)) {
-			$queryData['conditions'][$Model->alias . '.' . $this->field] = '0';
+	function beforeFind($model, $queryData) {
+		$queryData = array_merge(array('isDeleted' => false), $queryData);
+
+		if ($this->settings[$model->name]['enabled'] && $model->hasField($this->field) && isset($queryData['isDeleted'])) {
+			if ($queryData['isDeleted']) {
+				$queryData['conditions']['NOT'][$model->alias . '.' . $this->field] = $this->notDeleted($model);
+			} else {
+				$queryData['conditions'][$model->alias . '.' . $this->field] = $this->notDeleted($model);
+			}
 		}
 		return $queryData;
 	}
+/**
+ * Disables soft deletable behavior for a specific model.
+ * 
+ * @param object $model Model instance
+ * @return null
+ */
+	function disableDeletable(&$model) {
+		$this->settings[$model->name]['enabled'] = false;
+	}
+/**
+ * Enables soft deletable behavior for a specific model.
+ * 
+ * @param object $model Model instance
+ * @return null
+ */
+	function enableDeletable(&$model) {
+		$this->settings[$model->name]['enabled'] = true;
+	}
+/**
+ * Restore a previously soft deleted record.
+ * 
+ * @param object $model Model instance
+ * @param numeric $id Id of record to restore
+ * @return boolean True if restore was successful, false if otherwise
+ */
+	function restore(&$model, $id = null) {
+		if (is_numeric($id)) {
+			$model->id = $id;
+		}
+		if (!$model->id) {
+			return false;
+		}
+		$model->disableDeletable();
+		if ($model->exists() && $model->beforeRestore()) {
+			if ($this->update($model, $this->notDeleted($model))) {
+				$model->afterRestore();
+				$model->enableDeletable();
+				return true;
+			}
+		}		
+		$model->enableDeletable();
+		return false;
+	}
+/**
+ * Restore callbacks to be overridden by models.
+ * 
+ */	
+	function beforeRestore() {
+		return true;
+	}
+	function afterRestore() {
+	}
+/**
+ * Determine the value to set in the deleted field to based on column type in schema.
+ * 
+ * @param object $model Model instance
+ * @return mixed
+ * @access private
+ */	
+	private function deleted($model) {
+		if ($model->_schema[$this->field]['type'] == 'datetime') {
+			return date('c');
+		}
+		return true;
+	}	
+	private function notDeleted($model) {
+		if ($model->_schema[$this->field]['type'] == 'datetime') {
+			return null;
+		}
+		return false;
+	}
+/**
+ * Update the soft deletable field with a new value.
+ * 
+ * @param object $model Model instance
+ * @param numeric $value New value for field
+ * @access private
+ */	
+	private function update(&$model, $value) {
+		return $model->save(
+			array($model->alias => array($this->field => $value)), 
+			array('validate' => false, 'callbacks' => true)
+		);
+	}
 }
-?>
